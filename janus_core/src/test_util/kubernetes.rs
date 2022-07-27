cf996b83bcfa38ce32191f32905d1fc195185385
Tim Geoghegan <timg@letsencrypt.org>
2022-07-27 12:48:46 -0700

    Allow integration tests against k8s cluster
    Extends test support in `monolithic_integration_test` so that tests can
    be run against Janus instances already running in a Kubernetes cluster.
    The cluster could either be running locally in Kind, or remotely in a
    cloud platform.
    
    It's assumed that two Janus instances are running in the cluster, in
    distinct Kubernetes namespaces. It's also assumed that there exist
    Kubernetes services named `aggregator` and `postgresql` for the DAP API
    and the datastore, respectively.
    
    Parameters needed to connect to the Kubernetes cluster are provided to
    the test in environment variables, documented in `JanusPair::new`. If
    the environment variables are empty, then the test is run against
    ephemeral, in-process Janus components.
    
    Part of https://github.com/divviup/janus-ops/issues/39,
    https://github.com/divviup/janus-ops/issues/141

--- janus_core/src/test_util/kubernetes.rs
+++ janus_core/src/test_util/kubernetes.rs
@@ -2,21 +2,92 @@
 
 use kube::config::{KubeConfigOptions, Kubeconfig};
 use rand::{thread_rng, Rng};
-use std::process::{Command, Stdio};
-use tempfile::{NamedTempFile, TempPath};
+use std::{
+    path::{Path, PathBuf},
+    process::{Child, Command, Stdio},
+};
+use tempfile::NamedTempFile;
+use tracing::debug;
+
+/// Cluster represents a running Kubernetes cluster.
+pub struct Cluster {
+    /// Path to the Kubernetes config file, e.g., `~/.kube/config`
+    kubeconfig_path: PathBuf,
+    /// Name of the `kubectl` context to use.
+    context_name: String,
+}
+
+impl Cluster {
+    /// Creates a handle to an existing Kubernetes cluster.
+    pub fn create<P: AsRef<Path>>(kubeconfig_path: P, context_name: &str) -> Self {
+        let kubeconfig_path = kubeconfig_path.as_ref();
+        debug!(?kubeconfig_path, context_name, "Creating cluster handle");
+        Self {
+            kubeconfig_path: kubeconfig_path.to_path_buf(),
+            context_name: context_name.to_owned(),
+        }
+    }
+
+    /// Returns a new [`kube::Client`] configured to interact with this Kubernetes cluster.
+    pub async fn client(&self) -> kube::Client {
+        kube::Client::try_from(
+            kube::Config::from_custom_kubeconfig(
+                Kubeconfig::read_from(&self.kubeconfig_path).unwrap(),
+                &KubeConfigOptions {
+                    context: Some(self.context_name.clone()),
+                    ..KubeConfigOptions::default()
+                },
+            )
+            .await
+            .unwrap(),
+        )
+        .unwrap()
+    }
+
+    /// Set up port forwarding from `local_port` to `service_port` on the service in the namespace.
+    /// Returns a [`PortForwardGuard`] which will kill the child `kubectl` process on drop.
+    pub fn forward_port(
+        &self,
+        namespace: &str,
+        service: &str,
+        local_port: u16,
+        service_port: u16,
+    ) -> PortForwardGuard {
+        let args = [
+            "--kubeconfig",
+            &self.kubeconfig_path.to_string_lossy(),
+            "--context",
+            &self.context_name,
+            "--namespace",
+            namespace,
+            "port-forward",
+            &format!("service/{service}"),
+            &format!("{local_port}:{service_port}"),
+        ];
+        debug!(?args, "Invoking kubectl");
+        let child = Command::new("kubectl")
+            .args(args)
+            .stdin(Stdio::null())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .unwrap();
+
+        PortForwardGuard { child }
+    }
+}
 
 /// EphemeralCluster represents a running ephemeral Kubernetes cluster for testing. Dropping an
 /// EphemeralCluster will cause the associated Kubernetes cluster to be stopped & cleaned up.
 pub struct EphemeralCluster {
-    name: String,
-    kubeconfig_path: TempPath,
+    pub cluster: Cluster,
 }
 
 impl EphemeralCluster {
     /// Creates & starts a new ephemeral Kubernetes cluster.
     pub fn create() -> Self {
         // Choose a temporary file location for our kube config.
-        let kubeconfig_path = NamedTempFile::new().unwrap().into_temp_path();
+        let kubeconfig_path = NamedTempFile::new().unwrap().into_temp_path().to_path_buf();
 
         // Choose a cluster name.
         let mut randomness = [0u8; 4];
@@ -47,25 +118,9 @@ impl EphemeralCluster {
             .success());
 
         Self {
-            name: cluster_name,
-            kubeconfig_path,
+            // Kind prefixes the cluster name with "kind-" when creating a kubectl context
+            cluster: Cluster::create(kubeconfig_path, &format!("kind-{cluster_name}")),
         }
-    }
-
-    /// Returns a new [`kube::Client`] configured to interact with this Kubernetes cluster.
-    pub async fn client(&self) -> kube::Client {
-        kube::Client::try_from(
-            kube::Config::from_custom_kubeconfig(
-                Kubeconfig::read_from(&self.kubeconfig_path).unwrap(),
-                &KubeConfigOptions {
-                    context: Some(format!("kind-{}", self.name)),
-                    ..KubeConfigOptions::default()
-                },
-            )
-            .await
-            .unwrap(),
-        )
-        .unwrap()
     }
 }
 
@@ -77,9 +132,9 @@ impl Drop for EphemeralCluster {
                 "delete",
                 "cluster",
                 "--kubeconfig",
-                &self.kubeconfig_path.to_string_lossy(),
+                &self.cluster.kubeconfig_path.to_string_lossy(),
                 "--name",
-                &self.name,
+                &self.cluster.context_name,
             ])
             .stdin(Stdio::null())
             .stdout(Stdio::null())
@@ -87,6 +142,22 @@ impl Drop for EphemeralCluster {
             .status()
             .unwrap()
             .success())
+    }
+}
+
+/// A guard on a running `kubectl port-forward` process. The process will be killed and reaped when
+/// the guard is dropped.
+pub struct PortForwardGuard {
+    child: Child,
+}
+
+impl Drop for PortForwardGuard {
+    fn drop(&mut self) {
+        debug!(?self.child, "dropping port forward");
+        // We kill and reap child `kubectl` processes here to ensure that we don't create zombies on
+        // test failures. This is normally a questionable practice, but acceptable since this is
+        // only used in test code.
+        self.child.kill().unwrap();
     }
 }
 
@@ -105,7 +176,7 @@ mod tests {
         install_test_trace_subscriber();
 
         let first_cluster = EphemeralCluster::create();
-        let first_client = first_cluster.client().await;
+        let first_client = first_cluster.cluster.client().await;
         let first_nodes: Api<Node> = Api::all(first_client);
         assert_eq!(
             first_nodes
@@ -117,7 +188,7 @@ mod tests {
         );
 
         let second_cluster = EphemeralCluster::create();
-        let second_client = second_cluster.client().await;
+        let second_client = second_cluster.cluster.client().await;
         let second_nodes: Api<Node> = Api::all(second_client);
         assert_eq!(
             second_nodes
